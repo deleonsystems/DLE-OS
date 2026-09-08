7c363a1029187038c6f12998aa4ad7be3ce7bffb
Adan Mena <adan@dlemfg.com>
2026-09-08 16:22:16 -0700

    feat(ui): refine responsive header presentation

--- SRC/home/work-area-home.js
+++ SRC/home/work-area-home.js
@@ -6,7 +6,7 @@
   function greeting() {
     const hour = new Date().getHours();
     if (hour < 12) return "Good morning";
-    if (hour < 18) return "Good afternoon";
+    if (hour < 18) return "Good Afternoon";
     return "Good evening";
   }
 
